ca7fc0940811b4a2c7333a9668bf54c99cadce3c
Alison <test@test.com>
2018-06-07 12:04:57 -0400

    player class

--- components/game.py
+++ components/game.py
@@ -1,3 +1,47 @@
 '''
 manages different components of the game
 '''
+
+class Game:
+
+	def __init__(self):
+		pass
+
+	def set_up(self):
+		'''
+		Initialize all the components of the game 
+		i.e. all the parts that happen before any players
+		make decisions
+		'''
+		pass
+
+	def phase_one(self):
+		'''
+		determine player order
+		'''
+		pass
+
+	def phase_two(self):
+		'''
+		auction power plants
+		'''
+		pass
+
+	def phase_three(self):
+		'''
+		buy resources
+		'''
+		pass
+
+	def phase_four(self):
+		'''
+		build generators
+		'''
+		pass
+
+	def phase_five(self):
+		'''
+		bureaucracy
+		'''
+		pass
+		'''
---
+++ components/player.py
@@ -0,0 +1,8 @@
+
+
+class Player:
+
+	def __init__(self, player_id, money=50):
+		self.player_id = player_id
+		self.money = money
+		self.powerplants = []
